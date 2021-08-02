b0f0af65f756027968614a201ed19203880510ae
Antonio D <tmitetime@gmail.com>
2021-08-02 15:14:25 -0400

    Added everything upto current user next is collections

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   root "main#index"
   resources :main
   resources :posts
+  
+  get "register", to: "register#new"
+  post "register", to: "register#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
