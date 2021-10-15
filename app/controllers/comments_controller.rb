560605d12cca651c455099ed105998f392a3453d
Antonio D <tmitetime@gmail.com>
2021-10-15 15:47:57 -0400

    All before postgres switch

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,19 @@
 class CommentsController < ApplicationController 
+    before_action :find_post, only: [:create]
     def create
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(params[:comment].permit(:email, :body))
-        redirect_to post_path(@post)
+        if Current.user != nil            
+            @comment = @post.comments.create(params[:comment].permit(:email, :body))
+            redirect_to post_path(@post)
+        else
+            redirect_to post_path(@post), alert: "Please sign in or create an account"
+        end
     end
 
     private
     def newComment
         @comment = Comment.new
+    end
+    def find_post
+        @post = Post.find(params[:post_id])
     end
 end
---
+++ db/migrate/20211015031903_create_all_tags_text.rb
@@ -0,0 +1,5 @@
+class CreateAllTagsText < ActiveRecord::Migration[6.1]
+  def change
+    add_column :posts, :AllTags, :string
+  end
+end
