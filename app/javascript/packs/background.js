bb906e22e1f62aef88819d3e3e03c48ff94dc13b
Antonio D <tmitetime@gmail.com>
2021-10-29 21:09:47 -0400

    JavaScript attempt

---
+++ app/javascript/packs/background.js
@@ -0,0 +1,8 @@
+let colors = ['blue', 'yellow, "red', "green"];
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', function(){
+    console.log('Button')
+})
+   
