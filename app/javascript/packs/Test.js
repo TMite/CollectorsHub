153c8ec0b84819298db3feb8a281e8350258010b
Antonio D <tmitetime@gmail.com>
2021-11-02 14:33:10 -0400

    Working Background toggle from green green/blue

--- app/javascript/packs/Test.js
+++ app/javascript/packs/Test.js
@@ -1,8 +1,34 @@
-let colors = ['blue', 'green', 'yellow', 'red'];
-let button = document.getElementById("button");
+let BlueMode = localStorage.getItem('BlueMode')
+let button = document.querySelector('#button');
 
+const enableBlue = () =>{
+    document.body.classList.add("background-Blue");   
+    localStorage.setItem('BlueMode','enabled');
+};
+const disableBlue = () =>{
+    document.body.classList.remove("background-Blue");
+    
+    localStorage.setItem('BlueMode', null);
+};
+
+if(BlueMode === 'enabled')
+{
+    enableBlue();
+}
 button.addEventListener('click', function(){
-    var rand = colors[Math.floor(Math.random() * colors.length)];
-    let background = document.getElementById("background");
-    background.style.background = rand;
+    //Comment
+    // var rand = colors[Math.floor(Math.random() * colors.length)];
+    // let background = document.getElementById("background");
+    // background.style.background = rand;
+    BlueMode = localStorage.getItem('BlueMode');
+    console.log('background');
+    if(BlueMode !== 'enabled')
+    {
+        enableBlue();
+    }
+    else
+    {
+        disableBlue();
+    }
+
 })
